cb5cc05d98e4ca2fb6d54187b1896928ed9dca2d
ntisbaen <titissuk@gmail.com>
2023-05-10 01:35:19 -0700

    Initial commit

---
+++ src/main/java/com/mycompany/pertemuan05/RINCIAN.java
@@ -0,0 +1,28 @@
+
+package com.mycompany.pertemuan05;
+
+public class RINCIAN {
+           public static void main(String[] args){
+    
+        TOKO Titis =new TOKO();
+       Titis.merek = "asus ROG";
+        Titis.harga=1500000;
+        
+         TOKO Titis1 =new TOKO();
+       Titis1.merek = "MAC BOOK";
+        Titis1.harga=2000000;
+        
+          TOKO Titis2 =new TOKO();
+       Titis2.merek = "ACER GAMING";
+        Titis2.harga=1300000;
+        
+        Titis.ingfo();
+         Titis1.ingfo();
+          Titis2.ingfo();
+    
+    
+    
+    
+    }
+    
+}
---
+++ src/main/java/com/mycompany/pertemuan05/TOKO.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.pertemuan05;
+
+
+public class TOKO {
+        String merek;
+    int harga;
+    
+    void ingfo(){
+         System.out.println("=========================== ");
+        System.out.println("NAMA MEREK : "+merek);
+        
+         System.out.println("HARGA JUAL :" +harga);
+          System.out.println("=========================== ");
+    }
+}
